8839e3c22dd6f47407264a755a8c408034b6194e
Natasha Njili <natashanjili@gmail.com>
2025-09-28 21:35:45 +0200

    appointment fixed

--- src/main/java/za/ac/cput/Repository/AppointmentRepository.java
+++ src/main/java/za/ac/cput/Repository/AppointmentRepository.java
@@ -1,6 +1,7 @@
 package za.ac.cput.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.Domain.Appointment;
 import java.time.LocalDate;
@@ -11,6 +12,7 @@ import java.util.List;
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
     // Find all appointments for a specific DOCTOR on a specific DATE
+    @Query("SELECT a FROM Appointment a WHERE a.doctor.userId= :doctorId AND a.appointmentDate = :date")
     List<Appointment> findByDoctorIdAndAppointmentDate(Long doctorId, LocalDate appointmentDate);
 
 }
